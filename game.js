929a37bcc6b32b23776ee06e4c5e4c73b2ff0741
Ryan Lester <lestertr@dukes.jmu.edu>
2020-08-09 16:53:23 -0400

    bombs and valid squares randomly sorted and bombs appear as orange sqs

--- game.js
+++ game.js
@@ -9,12 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
         //get shuffled game array with random bombs
         const bombsArray = Array(bombAmount).fill('bomb')
         const emptyArray = Array(width*width - bombAmount).fill('valid')
-        console.log(bombsArray)
-        console.log(emptyArray)
+        const gameArray = emptyArray.concat(bombsArray)
+        const shuffledArray = gameArray.sort( () => Math.random() - 0.5)
 
         for(let i = 0; i < width*width; i++) {
             const square = document.createElement('div') 
             square.setAttribute('id', i)
+            square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
         }
