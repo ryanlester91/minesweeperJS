6a1512726ce10b3927bf2f8e29d28563b2cf640a
Ryan Lester <lestertr@dukes.jmu.edu>
2020-08-09 17:19:04 -0400

    completed for-loop if conditions logic for bomb and valid squares placement

--- game.js
+++ game.js
@@ -18,7 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
+
+            //normal click
+            square.addEventListener()
         }
+
+        //add numbers
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0
+            const isLeftEdge = (i % width === 0)
+            const isRightEdge = (i % width === width - 1)
+            
+            
+            if (squares[i].classList.contains('valid')) {
+                if(i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
+                if(i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++
+                if(i > 10 && squares[i -width].classList.contains('bomb')) total ++
+                if(i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total ++
+                if(i > 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total ++
+                if(i > 90 && squares[i -1 +width].classList.contains('bomb')) total ++
+                if(i > 88 && !isRightEdge && squares[i +1 +width].classList.contains('bomb')) total ++
+                if(i > 80 && squares[i +width].classList.contains('bomb')) total ++
+                squares[i].setAttribute('bacon', total)
+                console.log(squares[i])
+            }
+        }
+
+
+
+
+
     }
 
     createBoard();
